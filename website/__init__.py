ed6973e8dbd71c3c0ff1b228e467ab38976f0aa3
thcorreia <43270885+thcorreia@users.noreply.github.com>
2022-06-05 10:38:49 +0200

    DB2

--- website/__init__.py
+++ website/__init__.py
@@ -19,8 +19,9 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
-    from .models import User, Notes 
+   
+    from .models import User, Note
+    
 
     create_database(app)
     
--- website/auth.py
+++ website/auth.py
@@ -22,8 +22,7 @@ def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         userName = request.form.get('userName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2') 
+        password1 = request.form.get('password1') 
 
         if len(email) < 4:
             flash('email must be greater than 4 characters', category='error')
@@ -31,17 +30,17 @@ def sign_up():
             flash('User name should be greater than 4 characters', category='error')
         elif len(password1) < 7:
             flash('Passwords must be at least 7 characters', category='error')
-        #elif password1 != password2:
-         #   flash('Passwords don\'t match.', category='error')
+        
         else:
+            #new_user = User( email = email, userName = userName, password1=generate_password_hash(
+             #   password1, method='sha256')) 
+            #db.session.add(new_user)
+           # db.session.commit()   
             flash('Account created!', category='success') 
-            return redirect(url_for('views.home')) #redirecionar para a pagina de dashboard 
-            #add user to DB 
+            return redirect(url_for('views.home')) 
 
     return render_template('sign_up.html')
 
-@auth.route("/logout")
-def logout():
-    return render_template('logout.html') 
+ 
 
 
